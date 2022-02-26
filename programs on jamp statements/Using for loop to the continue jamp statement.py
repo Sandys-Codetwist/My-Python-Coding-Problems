ec9af71badfcfa280ff9cbde502a21370b99166b
Codetwist <99180218+Sandys-Codetwist@users.noreply.github.com>
2022-02-26 11:19:01 +0530

    Add files via upload

---
+++ programs on jamp statements/Using for loop to the continue jamp statement.py
@@ -0,0 +1,6 @@
+str_="python"
+for i in str_:
+	if i=="t":
+		continue
+ 
+	print(i)    
---
+++ programs on jamp statements/while loop for continue jamp statement.py
@@ -0,0 +1,6 @@
+i=0
+while i<10:
+	i=i+1
+	if i==4:
+		continue # It is used to skip some specific code for a perticular condtion  
+	print(i)
